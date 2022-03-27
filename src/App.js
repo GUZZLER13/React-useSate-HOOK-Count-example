5c674a867a80c235a43184a44f6a6d51bc3b4f64
guzzler13 <stephanedasil@gmail.com>
2022-03-27 22:00:29 +0200

    p => h1 and h2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
 
 function App() {
+  const [count, setCount] = useState(14)
+  const [clickNumber, setclickNumber] = useState(0)
+  const [countReset] = useState(0)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Le compteur est à {count} </h1>
+      <button
+        onClick={() => {
+          setCount(count + 1)
+          setclickNumber(clickNumber + 1)
+        }}
+      >
+        Augmenter compteur
+      </button>
+      <button
+        onClick={() => {
+          setCount(count - 1)
+          setclickNumber(clickNumber + 1)
+        }}
+      >
+        Diminuer compteur
+      </button>
+      <button
+        onClick={() => {
+          setCount(countReset)
+          setclickNumber(clickNumber + 1)
+        }}
+      >
+        Reset Compteur
+      </button>
+      <button
+        onClick={() => {
+          setclickNumber(countReset)
+        }}
+      >
+        Reset nombre de Click
+      </button>
+      <button
+        onClick={() => {
+          setCount(countReset)
+          setclickNumber(countReset)
+        }}
+      >
+        Reset Compteur et nombre deClick
+      </button>
+      <h2>Tu as cliqué {clickNumber} fois</h2>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
